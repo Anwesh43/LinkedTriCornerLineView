3b3231d987cd2cb162f469243b489a5bf4346f5c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 02:26:31 +0530

    adding drawTCLNode canvas extension method

--- tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
+++ tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
@@ -27,3 +27,30 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + (scaleFactor()) * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+
+fun Canvas.drawTCLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val xGap : Float = size / lines
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawLine(-size / 2, 0f, 0f, -size / 2, paint)
+    drawLine(0f, -size / 2, size / 2, 0f, paint)
+    translate(-size / 2, 0f)
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float
+        save()
+        translate(j * xGap, -size / 2 * (1 - 2 * (j % 2)))
+        drawLine(0f, 0f, 0f, (-size / 2) * sc1.divideScale(j, lines) , paint)
+        restore()
+    }
+    restore()
+}
