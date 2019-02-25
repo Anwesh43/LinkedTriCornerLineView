42aaea063f65b82ae2d7543ab0d698dcd0b8c38e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 02:12:39 +0530

    adding constants

--- tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
+++ tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.tricornerlineview
 /**
  * Created by anweshmishra on 26/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#212121")
