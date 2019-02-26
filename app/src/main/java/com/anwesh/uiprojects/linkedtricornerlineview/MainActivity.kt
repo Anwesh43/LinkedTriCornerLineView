35fca170e38c07ebfb7e6660c5d89cb4b363e5bb
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 11:08:44 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtricornerlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtricornerlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtricornerlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.tricornerlineview.TriCornerLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriCornerLineView.create(this)
+        fullscreen()
     }
+}
+
+fun MainActivity.fullscreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
