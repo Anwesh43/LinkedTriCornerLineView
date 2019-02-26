77d5f9da16f473ca7207c848c3fee8ec1992b9cd
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 10:26:42 +0530

    adding Animator class

--- tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
+++ tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
@@ -91,4 +91,32 @@ class TriCornerLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    view.invalidate()
+                    Thread.sleep(50)
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
