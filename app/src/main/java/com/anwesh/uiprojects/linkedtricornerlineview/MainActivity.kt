2684dbb27244d8d95b62261b25bbb802384cb1b6
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 11:07:42 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtricornerlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtricornerlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtricornerlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tricornerlineview.TriCornerLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriCornerLineView.create(this)
     }
 }
