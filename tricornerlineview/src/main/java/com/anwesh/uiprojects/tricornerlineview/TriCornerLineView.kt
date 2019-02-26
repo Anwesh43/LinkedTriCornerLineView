37aab72bbd170a65cbac7434c86728a5f40b7221
Anwesh43 <anweshthecool0@gmail.com>
2019-02-26 11:21:04 +0530

    adding delay as constant

--- tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
+++ tricornerlineview/src/main/java/com/anwesh/uiprojects/tricornerlineview/TriCornerLineView.kt
@@ -20,6 +20,7 @@ val sizeFactor : Float = 2.9f
 val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#311B92")
 val backColor : Int = Color.parseColor("#212121")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -104,7 +105,7 @@ class TriCornerLineView(ctx : Context) : View(ctx) {
                 cb()
                 try {
                     view.invalidate()
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                 } catch(ex : Exception) {
 
                 }
